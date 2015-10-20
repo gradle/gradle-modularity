3a61e86ea5c40ff88b617584c44dba4da3b8ad35
hansd <hans@gradle.com>
2015-10-19 21:28:25 -0700

    Fix the build by removing reference to service internals

--- client/src/client/java/org.gradle.example.client/Client.java
+++ client/src/client/java/org.gradle.example.client/Client.java
@@ -1,12 +1,11 @@
 package org.gradle.example.client;
 
 import org.gradle.example.service.Service;
-import org.gradle.example.service.internal.ServiceUtils;
 
 public class Client {
     Service service;
 
     public void clientMethod() {
-        System.out.println(ServiceUtils.CONSTANT);
+        System.out.println("FOO");
     }
 }
