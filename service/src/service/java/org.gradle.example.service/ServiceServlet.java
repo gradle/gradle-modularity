abc65e4c54ec76d645a3908c20f6e4b8ed81a1ae
hansd <hans@gradle.com>
2015-10-19 21:52:08 -0700

    Add Jetty and HttpClient dependency to service and use them from within service.

---
+++ service/src/service/java/org.gradle.example.service/ServiceServlet.java
@@ -0,0 +1,6 @@
+package org.gradle.example.service;
+
+import org.eclipse.jetty.servlet.DefaultServlet;
+
+public class ServiceServlet extends DefaultServlet {
+}
--- service/src/service/java/org.gradle.example.service/internal/ServiceUtils.java
+++ service/src/service/java/org.gradle.example.service/internal/ServiceUtils.java
@@ -1,5 +1,12 @@
 package org.gradle.example.service.internal;
 
+import org.apache.http.client.methods.HttpGet;
+
 public class ServiceUtils {
     public static final String CONSTANT = "FOO";
+
+    public void doRequest() {
+        HttpGet httpget = new HttpGet(
+                "https://www.google.com/search?hl=en&q=gradle");
+    }
 }
